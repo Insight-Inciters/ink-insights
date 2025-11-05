69ce3f9837c9afc08f760ef33c3160502fa5c2d6
Jaskirat-Kaur-Gill <everythingrene@gmail.com>
2025-11-06 02:47:15 +1100

    Update dashboard.js

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -757,7 +757,7 @@ function showLeavePrompt() {
       });
     }, 400);
   };
-
+}
 // Detect cursor leaving viewport (intent to close tab)
 document.addEventListener("mouseleave", (e) => {
   if (e.clientY <= 0) showLeavePrompt();
